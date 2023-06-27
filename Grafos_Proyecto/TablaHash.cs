74a4cf0a8944d480943a6e3c8c30be128641a362
Jesus Noriega <jesusadan049@gmail.com>
2023-06-26 21:05:27 -0700

    creacion para el tamaño y en privado el funcionamiento del hash

--- Grafos_Proyecto/TablaHash.cs
+++ Grafos_Proyecto/TablaHash.cs
@@ -8,5 +8,20 @@ namespace Grafos_Proyecto
 {
     internal class TablaHash
     {
+        private Nodo[] elementos;
+        private int tamano;
+
+        public TablaHash(int tamano)
+        {
+            this.tamano = tamano;
+            elementos = new Nodo[tamano];
+        }
+
+        private int FuncionHash(string clave)
+        {
+            int hashCode = clave.GetHashCode();
+            int indice = Math.Abs(hashCode) % tamano;
+            return indice;
+        }
     }
 }
